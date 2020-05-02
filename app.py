ae7c8fe9e2480408cf30202f0cd274ac7ad1f2c6
Priyang Bhatt <64345839+PRIYANG-BHATT@users.noreply.github.com>
2020-05-02 19:55:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model_song.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+    
+
+    return render_template('index.html', prediction_text='You like :: {} Songs'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
